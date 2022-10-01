ae97abc398d4b15506397aaab76e701ebde3b24b
Silver Von <silver@Silvers-MacBook-Pro.local>
2022-10-01 13:27:09 +0800

    update theme control

--- src/modules/themeControl.js
+++ src/modules/themeControl.js
@@ -1,25 +1,20 @@
 export default function themeControl() {
-  const wrapper = document.createElement('div')
-  const checkbox = document.createElement('input')
+  const themeSwitch = document.createElement('input')
 
-  wrapper.className = 'theme_control_wrapper'
-  checkbox.className = 'theme_switch'
+  themeSwitch.type = 'checkbox'
+  themeSwitch.className = 'theme_switch'
 
-  checkbox.type = 'checkbox'
-
-  wrapper.appendChild(checkbox)
-
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.documentElement.setAttribute('theme', 'dark')
-    checkbox.checked = true
+    themeSwitch.checked = true
   } else {
     document.documentElement.setAttribute('theme', 'light')
-    checkbox.checked = false
+    themeSwitch.checked = false
   }
-  
-  checkbox.addEventListener('change', (e) => {
+
+  themeSwitch.addEventListener('change', (e) => {
     document.documentElement.setAttribute('theme', e.target.checked ? 'dark' : 'light')
   })
 
-  return wrapper
+  return themeSwitch
 }
